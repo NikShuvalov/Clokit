eff06fb1b274def5e8ef56253e709263e5eda977
NikShuvalov <nshuvalov89@gmail.com>
2017-03-04 11:11:29 -0500

    Added additional unit tests and methods to get the completion of a week after a given date as well as a method to get the end time of a given week.

--- app/src/main/java/shuvalov/nikita/clokit/AppUtils.java
+++ app/src/main/java/shuvalov/nikita/clokit/AppUtils.java
@@ -14,10 +14,10 @@ public class AppUtils {
     }
 
     /**
-     * Use this Method to get the Monday start of the week entered. Returns the most recent Monday 12:00:01am
+     * Use this Method to get the Monday start of the week entered.
      *
-     * @param weekNum Pass weekNum in YYYYW(W) format. First 4 digits is year, and week of the year is concatenated.
-     * @return
+     * @param weekNum Pass weekNum in YYYYW or YYYYWW format. First 4 digits are year, and week of the year is concatenated.
+     * @return Returns the most recent Monday 12:00:00:001am in millis.
      */
     public static long getWeekStartMillis(int weekNum){
         String weekNumAsString = String.valueOf(weekNum);
@@ -27,10 +27,45 @@ public class AppUtils {
         cal.set(Calendar.WEEK_OF_YEAR, weekNum);
         cal.set(Calendar.YEAR, yearNum);
         cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.SECOND, 1);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.DAY_OF_WEEK, 2);
+        cal.set(Calendar.MILLISECOND, 1);
+        return cal.getTimeInMillis();
+    }
+
+    /**
+     * Pass the weekNum to find the end of that week. This method is more reliable than the method that takes millis as parameter, and doesn't require a week start time.
+     *
+     * @param weekNum Pass WeekNum in YYYYW or YYYYWW format. First 4 digits are year, and week of the year is concatenated.
+     * @return Returns Sunday 11:59:59:999pm in millis of the weekNum passed.
+     */
+    public static long getWeekEndMillis(int weekNum){//More reliable
+        String weekNumAsString = String.valueOf(weekNum);
+        int yearNum = Integer.parseInt(weekNumAsString.substring(0,4));
+        weekNum = Integer.parseInt(weekNumAsString.substring(4));
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.WEEK_OF_YEAR, weekNum+1);
+        cal.set(Calendar.YEAR, yearNum);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.DAY_OF_WEEK, 2);
         cal.set(Calendar.MILLISECOND, 0);
-        return cal.getTimeInMillis();
+        return (cal.getTimeInMillis()-1);
+    }
+
+
+    /**
+     * This method is less reliable, because it's just hardcoded to add the number of milliseconds in a week - 2 to the millis passed.
+     * That being said, though it's less reliable, it's also more flexible if user sets there own week start time, instead of midnight Monday.
+     *
+     * @param weekStartTimeInMillis Pass the week start time in millis.
+     * @return Returns time in millis of 1 week after the milli time entered minus 2 milliseconds.
+     */
+    public static long getWeekEndMillis(long weekStartTimeInMillis){
+        return (weekStartTimeInMillis + 604799998); //Should return Sunday 11:59:59:999am if Monday 12:00:00:001am is passed.
+        //604799998 = milliseconds/week - 2;
+
     }
 }
--- app/src/test/java/shuvalov/nikita/clokit/AppUtilsUnitTest.java
+++ app/src/test/java/shuvalov/nikita/clokit/AppUtilsUnitTest.java
@@ -10,11 +10,45 @@ import static org.junit.Assert.*;
 public class AppUtilsUnitTest {
     @Test
     public void correctValueForWeekStart() throws Exception{
-        long firstMonday2017 = Long.valueOf("1483333201000");
-        long mlk2017 = Long.valueOf("1484542801000");
-        //ToDo: Add more tests for edge cases. Like last week of year and shit.
+        long firstMonday2017 = Long.valueOf("1483333200001");
+        long mlk2017 = Long.valueOf("1484542800001");
+        long birthday = Long.valueOf("1493006400001");
+        long xmas = Long.valueOf("1514178000001"); //Last week of the year, last monday of year
+
         assertEquals(firstMonday2017, AppUtils.getWeekStartMillis(20171));
         assertEquals(mlk2017, AppUtils.getWeekStartMillis(20173));
+        assertEquals(birthday, AppUtils.getWeekStartMillis(201717));
+        assertEquals(xmas, AppUtils.getWeekStartMillis(201752));
+    }
+
+    @Test
+    public void correctValueForWeeksEnd() throws Exception{
+        long firstMonday2017 = Long.valueOf("1483333200001");
+        long firstMondayEndOfWeek = Long.valueOf("1483937999999");
+
+        long mlk2017 = Long.valueOf("1484542800001");
+        long mlkWeekEnd = Long.valueOf("1485147599999");
+
+        long birthday = Long.valueOf("1493006400001");
+        long birthWeeksEnd = Long.valueOf("1493611199999");
+
+        long xmas = Long.valueOf("1514178000001");
+        long newYearsEve = Long.valueOf("1514782799999");
+
+
+        //Checks method with milli param
+        assertEquals(firstMondayEndOfWeek,AppUtils.getWeekEndMillis(firstMonday2017));
+        assertEquals(mlkWeekEnd, AppUtils.getWeekEndMillis(mlk2017));
+        assertEquals(birthWeeksEnd, AppUtils.getWeekEndMillis(birthday));
+        assertEquals(newYearsEve, AppUtils.getWeekEndMillis(xmas));
+
+        //CHecks method with weekNum param
+        assertEquals(firstMondayEndOfWeek, AppUtils.getWeekEndMillis(20171));
+        assertEquals(mlkWeekEnd, AppUtils.getWeekEndMillis(20173));
+        assertEquals(birthWeeksEnd, AppUtils.getWeekEndMillis(201717));
+        assertEquals(newYearsEve, AppUtils.getWeekEndMillis(201752));
+
+
 
 
     }
